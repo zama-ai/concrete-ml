74be01a968fce3130ab9f9b7edb3ad70ffb45a69
Benoit Chevallier-Mames <benoit.chevalliermames@zama.ai>
2022-01-24 18:38:37 +0100

    refactor: remove the return's in our custom assert functions
    closes #162

--- src/concrete/ml/common/debugging/custom_assert.py
+++ src/concrete/ml/common/debugging/custom_assert.py
@@ -1,7 +1,7 @@
 """Provide some variants of assert."""
 
 
-def _custom_assert(condition: bool, on_error_msg: str = "") -> None:
+def _custom_assert(condition: bool, on_error_msg: str = ""):
     """Provide a custom assert which is kept even if the optimized python mode is used.
 
     See https://docs.python.org/3/reference/simple_stmts.html#assert for the documentation
@@ -20,42 +20,33 @@ def _custom_assert(condition: bool, on_error_msg: str = "") -> None:
         raise AssertionError(on_error_msg)
 
 
-def assert_true(condition: bool, on_error_msg: str = "") -> None:
+def assert_true(condition: bool, on_error_msg: str = ""):
     """Provide a custom assert to check that the condition is True.
 
     Args:
         condition(bool): the condition. If False, raise AssertionError
         on_error_msg(str): optional message for precising the error, in case of error
 
-    Returns:
-        None
-
     """
-    return _custom_assert(condition, on_error_msg)
+    _custom_assert(condition, on_error_msg)
 
 
-def assert_false(condition: bool, on_error_msg: str = "") -> None:
+def assert_false(condition: bool, on_error_msg: str = ""):
     """Provide a custom assert to check that the condition is False.
 
     Args:
         condition(bool): the condition. If True, raise AssertionError
         on_error_msg(str): optional message for precising the error, in case of error
 
-    Returns:
-        None
-
     """
-    return _custom_assert(not condition, on_error_msg)
+    _custom_assert(not condition, on_error_msg)
 
 
-def assert_not_reached(on_error_msg: str) -> None:
+def assert_not_reached(on_error_msg: str):
     """Provide a custom assert to check that a piece of code is never reached.
 
     Args:
         on_error_msg(str): message for precising the error
 
-    Returns:
-        None
-
     """
-    return _custom_assert(False, on_error_msg)
+    _custom_assert(False, on_error_msg)
