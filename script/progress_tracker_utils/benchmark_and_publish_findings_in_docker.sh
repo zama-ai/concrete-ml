1b4ab5cd518d2557cd60df5a14a40d2817736b84
Arthur Meyre <arthur.meyre@zama.ai>
2022-02-14 16:19:52 +0100

    chore: bring back benchmarking
    - first step to debug benchmarks
    
    refs #128

--- script/progress_tracker_utils/benchmark_and_publish_findings_in_docker.sh
+++ script/progress_tracker_utils/benchmark_and_publish_findings_in_docker.sh
@@ -5,13 +5,6 @@
 
 set -e
 
-# shellcheck disable=SC1091
-if [ -f .env ]
-then
-    # shellcheck disable=SC1091
-    set -a; source .env; set +a
-fi
-
 DEV_VENV_PATH="/home/dev_user/dev_venv"
 
 # shellcheck disable=SC1090,SC1091
@@ -26,21 +19,12 @@ cd /src/ && make setup_env
 mkdir -p /tmp/keycache
 mkdir -p logs
 
-initial_ml_log=logs/$(date -u --iso-8601=seconds).ml.log
-make -s benchmark 2>&1 | tee -a "$initial_ml_log"
-
-final_ml_log=logs/$(date -u --iso-8601=seconds).ml.log
-cat -s "$initial_ml_log" | sed '1d; $d' > "$final_ml_log"
-
-# sed above removes the first and the last lines of the log
-# which are empty to provide a nice console output
-# but empty lines are useless for logs so we get rid of them
-
-rm "$initial_ml_log"
-cp "$final_ml_log" logs/latest.ml.log
+ml_log=logs/latest.ml.log
+rm -f "${ml_log}"
+make -s benchmark 2>&1 | tee -a "${ml_log}"
 
 curl \
-     -H 'Authorization: Bearer '"$ML_PROGRESS_TRACKER_TOKEN"'' \
-     -H 'Content-Type: application/json' \
-     -d @progress.json \
-     -X POST "$ML_PROGRESS_TRACKER_URL"/measurement
+-H "Authorization: Bearer ${ML_PROGRESS_TRACKER_TOKEN}" \
+-H "Content-Type: application/json" \
+-d @progress.json \
+-X POST "${ML_PROGRESS_TRACKER_URL}"/measurement
