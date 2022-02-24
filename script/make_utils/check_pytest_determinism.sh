23527939391420aab7d3d67d4f3f22995d5981a9
Benoit Chevallier-Mames <benoit.chevalliermames@zama.ai>
2022-02-24 17:10:07 +0100

    chore: show the problem in determinism if there is one
    closes #400

--- script/make_utils/check_pytest_determinism.sh
+++ script/make_utils/check_pytest_determinism.sh
@@ -6,8 +6,34 @@ RANDOMLY_SEED=$RANDOM
 echo "Testing determinism with seed $RANDOMLY_SEED"
 
 OUTPUT_DIRECTORY=$(mktemp -d)
+
+set +e
+
 RANDOMLY_SEED=$RANDOMLY_SEED TEST=tests/seeding/test_seeding.py make pytest_one_single_cpu > "${OUTPUT_DIRECTORY}/one.txt"
+
+# This would not be readable
+# SC2181: Check exit code directly with e.g. 'if mycmd;', not indirectly with $?.
+# shellcheck disable=SC2181
+if [ $? -ne 0 ]
+then
+    echo "The commandline failed with:"
+    cat  "${OUTPUT_DIRECTORY}/one.txt"
+    exit 255
+fi
+
 RANDOMLY_SEED=$RANDOMLY_SEED TEST=tests/seeding/test_seeding.py make pytest_one_single_cpu > "${OUTPUT_DIRECTORY}/two.txt"
+
+# This would not be readable
+# SC2181: Check exit code directly with e.g. 'if mycmd;', not indirectly with $?.
+# shellcheck disable=SC2181
+if [ $? -ne 0 ]
+then
+    echo "The commandline failed with:"
+    cat  "${OUTPUT_DIRECTORY}/two.txt"
+    exit 255
+fi
+
+set -e
 
 # Exceptions:
 #   passed in: since it is related to timings
