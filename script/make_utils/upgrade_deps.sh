f6477b19d4462d1b9d42d057eb13019af8b112c3
Arthur Meyre <arthur.meyre@zama.ai>
2022-03-08 17:19:26 +0100

    chore: update py-deps
    - also sort entries in the helper script as join complains

--- script/make_utils/upgrade_deps.sh
+++ script/make_utils/upgrade_deps.sh
@@ -7,8 +7,8 @@ no_dev_file=$(mktemp --suffix=.txt)
 all_file=$(mktemp --suffix=.txt)
 dev_file=$(mktemp --suffix=.txt)
 
-poetry show -o -t --no-dev | grep -v -e "--" | cut -d " " -f 1 | sed 's/$/\@latest/g' > "${no_dev_file}"
-poetry show -o -t | grep -v -e "--" | cut -d " " -f 1 | sed 's/$/\@latest/g' > "${all_file}"
+poetry show -o -t --no-dev | grep -v -e "--" | cut -d " " -f 1 | sed 's/$/\@latest/g' | sort > "${no_dev_file}"
+poetry show -o -t | grep -v -e "--" | cut -d " " -f 1 | sed 's/$/\@latest/g' | sort > "${all_file}"
 join -v1 -v2 "${all_file}" "${no_dev_file}" > "${dev_file}"
 # shellcheck disable=SC2002
 cat "${no_dev_file}" | xargs poetry add
