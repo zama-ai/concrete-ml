fe80aa663ae034599889926ac1f1e09a52104339
Arthur Meyre <arthur.meyre@zama.ai>
2022-03-29 18:42:35 +0200

    chore: fix codeblocks tests by deactivating admonitions in a temp dir

--- Makefile
+++ Makefile
@@ -333,13 +333,25 @@ upgrade_py_deps:
 .PHONY: pytest_codeblocks # Test code blocks using pytest in the documentation
 pytest_codeblocks:
 	@# grep -v "^\./\." is to avoid files in .hidden_directories
-	find . -type f -name "*.md" | grep -v "^\./\." | \
-	xargs poetry run pytest --codeblocks -svv \
+	MD_FILES=($$(find . -type f -name "*.md" | grep -v "^\./\.")) && \
+	TEST_DIR="/tmp/cml_codeblocks" && \
+	rm -rf "$${TEST_DIR}" && \
+	mkdir -p "$${TEST_DIR}" && \
+	DEST_MDS=() && \
+	for MD_FILE in $${MD_FILES[@]}; do \
+		DEST_MD="$${TEST_DIR}/$${MD_FILE}"; \
+		mkdir -p "$$(dirname $${DEST_MD})"; \
+		cp "$${MD_FILE}" "$${DEST_MD}"; \
+		DEST_MDS+=("$${DEST_MD}"); \
+	done && \
+	poetry run python ./script/make_utils/deactivate_docs_admonitions_for_tests.py \
+	--files $${DEST_MDS[@]} && \
+	poetry run pytest --codeblocks -svv \
 	--capture=tee-sys \
 	-n $$(./script/make_utils/ncpus.sh) \
 	--randomly-dont-reorganize \
 	--count=$(COUNT) \
-	--randomly-dont-reset-seed
+	--randomly-dont-reset-seed "$${TEST_DIR}" && rm -rf "$${TEST_DIR}" || rm -rf "$${TEST_DIR}"
 
 # From https://stackoverflow.com/a/63523300 for the find command
 .PHONY: shell_lint # Lint all bash scripts
---
+++ script/make_utils/deactivate_docs_admonitions_for_tests.py
@@ -0,0 +1,50 @@
+"""Helper script to disable admonitions but keep line numbers untouched."""
+
+import argparse
+import multiprocessing
+import re
+import sys
+from pathlib import Path
+
+
+def process_file(file_str: str):
+    """Deactivate admonitions in a file.
+
+    Args:
+        file_str (str): the path to the file to process.
+    """
+    file_path = Path(file_str).resolve()
+    print(f"Removing admonitions for: {str(file_path)}")
+    file_content = None
+    with open(file_path, "r", encoding="utf-8") as f:
+        file_content = "".join(f.readlines())
+
+    # Check https://regex101.com/ with python gms flags to understand why this works
+    processed_content = re.sub(
+        r"(```{)(.*?)(```)", r"ignore\1\2ignore\3", file_content, flags=re.DOTALL
+    )
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(processed_content)
+
+    return True
+
+
+def main(args):
+    """Entry point."""
+    with multiprocessing.Pool(multiprocessing.cpu_count()) as pool:
+        res = pool.map(process_file, args.files)
+        # Exit 0 if all went well as True == 1
+        sys.exit(not all(res))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        "Deactivate admonitions without changing line numbers", allow_abbrev=False
+    )
+
+    parser.add_argument(
+        "--files", type=str, nargs="+", required=True, help="The files to modify in place"
+    )
+
+    cli_args = parser.parse_args()
+    main(cli_args)
