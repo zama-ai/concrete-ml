0af089cdcfe920690bdccb59fe7988964aff6e66
Benoit Chevallier-Mames <benoit.chevalliermames@zama.ai>
2022-07-21 19:16:49 +0200

    chore: test that --forcing_random_seed allows us to reproduce part of a full test with correct seeds
    closes #1215

--- script/make_utils/check_pytest_determinism.sh
+++ script/make_utils/check_pytest_determinism.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 RANDOMLY_SEED=$RANDOM
 echo "Testing determinism with seed $RANDOMLY_SEED"
@@ -39,3 +39,39 @@ set -e
 #   passed in: since it is related to timings
 diff "${OUTPUT_DIRECTORY}/one.txt" "${OUTPUT_DIRECTORY}/two.txt" -I "passed in"
 echo "Successful determinism check"
+
+# Now, check --forcing_random_seed, ie check that one can reproduce conditions of a bug in a single file
+# and test without having to relaunch the full pytest, by just picking the right --forcing_random_seed
+
+LIST_FILES=$(grep "tests/seeding/test_seeding.py" "${OUTPUT_DIRECTORY}/one.txt")
+LIST_SEED=()
+while IFS='' read -r line; do LIST_SEED+=("$line"); done < <(grep "forcing_random_seed" "${OUTPUT_DIRECTORY}/one.txt" | sed -e "s@Relaunch the tests with --forcing_random_seed @@" | sed -e "s@ --randomly-dont-reset-seed to reproduce@@")
+
+WHICH=0
+echo "" > "${OUTPUT_DIRECTORY}/three.txt"
+for x in $LIST_FILES
+do
+    # For test test_seed_3, we need to add $RANDOMLY_SEED
+    EXTRA_OPTION=""
+    if [ "$x" == "tests/seeding/test_seeding.py::test_seed_3[random_inputs0]" ]
+    then
+        EXTRA_OPTION=" --randomly-seed=$RANDOMLY_SEED"
+    fi
+
+    echo "poetry run pytest $x -xsvv $EXTRA_OPTION --randomly-dont-reset-seed --forcing_random_seed " "${LIST_SEED[WHICH]}"
+
+    # Only take lines after the header, ie after line with 'collecting'
+    # SC2086 is about double quote to prevent globbing and word splitting, but here, it makes that we have
+    # an empty arg in pytest, which is considered as "do pytest for all files"
+    # shellcheck disable=SC2086
+    poetry run pytest "$x" -xsvv $EXTRA_OPTION --randomly-dont-reset-seed --forcing_random_seed "${LIST_SEED[WHICH]}" | sed -n -e '/collecting/,$p' | grep -v collecting | grep -v "passed in" | grep -v "PASSED" >> "${OUTPUT_DIRECTORY}/three.txt"
+
+    ((WHICH+=1))
+done
+
+# Clean a bit one.txt
+sed -n -e '/collecting/,$p' "${OUTPUT_DIRECTORY}/one.txt" | grep -v collecting | grep -v "passed in" | grep -v "PASSED" | grep -v "Leaving directory" > "${OUTPUT_DIRECTORY}/one.modified.txt"
+
+echo ""
+diff "${OUTPUT_DIRECTORY}/one.modified.txt" "${OUTPUT_DIRECTORY}/three.txt" --ignore-all-space --ignore-blank-lines --ignore-space-change
+echo "Successful --forcing_random_seed check"
