9d45e0e1f95ad7e32613dbbd309218ec6cbfe7c1
Andrei Stoian <andrei.stoian@zama.ai>
2022-09-05 10:06:03 +0200

    chore: fix github cli installation in CI

--- script/make_utils/setup_os_deps.sh
+++ script/make_utils/setup_os_deps.sh
@@ -84,11 +84,8 @@ linux_install_github_cli () {
     # Installs github cli
     # https://github.com/cli/cli/blob/trunk/docs/install_linux.md#debian-ubuntu-linux-raspberry-pi-os-apt
     echo "Installing github-CLI"
-    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-    chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-    apt update
-    apt install gh
+    wget https://github.com/cli/cli/releases/download/v2.14.7/gh_2.14.7_linux_amd64.deb
+    dpkg -i gh_2.14.7_linux_amd64.deb
 }
 
 OS_NAME=$(uname)
