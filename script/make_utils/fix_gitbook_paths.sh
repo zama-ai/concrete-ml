985d0d5a57d8c7599cc62132862a2a82eeab5f56
Benoit Chevallier-Mames <benoit.chevalliermames@zama.ai>
2022-09-26 12:32:08 +0200

    docs: integration of lazydocs
    for the API documentations in GitBook
    closes #1798

---
+++ script/make_utils/fix_gitbook_paths.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ex
+
+DIRECTORY=$1
+
+FILES=$(find "$DIRECTORY" -name "*.md")
+
+for f in ${FILES}
+do
+    # sed -i is different on mac and on linux
+    sed "s@../developer-guide/api@https://github.com/zama-ai/concrete-ml-internal/tree/main/docs/developer-guide/api@g" "$f" > tmp.file.md
+    mv tmp.file.md "$f"
+done
