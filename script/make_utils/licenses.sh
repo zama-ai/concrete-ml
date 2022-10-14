83980fd22ed536d3be44ca8a3819aa86b9eb624b
Luis Montero <luis.montero@zama.ai>
2022-10-14 14:58:31 +0200

    chore: change brevitas version, git to pypi.
    closes #1935

--- script/make_utils/licenses.sh
+++ script/make_utils/licenses.sh
@@ -156,7 +156,7 @@ then
     LICENSES_WHITELIST="${LICENSES_WHITELIST};Other/Proprietary License"
     LICENSES_WHITELIST="${LICENSES_WHITELIST};Python Software Foundation License"
 
-    pip-licenses --allow-only="${LICENSES_WHITELIST}" --ignore-packages brevitas pkg-resources
+    pip-licenses --allow-only="${LICENSES_WHITELIST}" --ignore-packages brevitas pkg-resources concrete-ml-extensions-brevitas
 
     deactivate
 
