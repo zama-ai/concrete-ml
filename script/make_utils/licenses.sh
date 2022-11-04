8d86489815fd9c00eead75526f53ddcb15332569
Benoit Chevallier-Mames <benoit.chevalliermames@zama.ai>
2022-11-04 10:14:29 +0100

    chore: remove once for all dev licenses

--- script/make_utils/licenses.sh
+++ script/make_utils/licenses.sh
@@ -9,9 +9,6 @@ DIFF_TOOL="diff --ignore-all-space --ignore-tab-expansion --ignore-space-change 
 TMP_VENV_PATH="/tmp/tmp_venv"
 DO_USER_LICENSES=1
 
-# Dev licences are not done, but could be re-enabled
-DO_DEV_LICENSES=0
-
 OUTPUT_DIRECTORY="${LICENSE_DIRECTORY}"
 DO_FORCE_UPDATE=0
 CN_VERSION="concrete-numpy[full]"
@@ -169,46 +166,6 @@ then
     else
         # Update the .md5 files
         openssl md5 poetry.lock > ${LICENSES_FILENAME}.md5
-    fi
-fi
-
-if [ $DO_DEV_LICENSES -eq 1 ]
-then
-    # Licenses for developer (install in a temporary venv)
-    echo "Doing licenses for developper"
-    echo "Warning: contrarily to the user licences, there is currently no early abort if "
-    echo "poetry.lock hasn't changed (in the dev licences). Please update"
-
-    FILENAME="${BASENAME}_${OS}_dev.txt"
-    LICENSES_FILENAME="${LICENSE_DIRECTORY}/${FILENAME}"
-    NEW_LICENSES_FILENAME="${OUTPUT_DIRECTORY}/${FILENAME}"
-
-    rm -rf $TMP_VENV_PATH/tmp_venv
-    python3 -m venv $TMP_VENV_PATH/tmp_venv
-
-    # SC1090: Can't follow non-constant source. Use a directive to specify location.
-    # shellcheck disable=SC1090,SC1091
-    source $TMP_VENV_PATH/tmp_venv/bin/activate
-
-    make setup_env
-    pip-licenses | grep -v "pkg\-resources\|concrete-ml" | tee "${NEW_LICENSES_FILENAME}"
-
-    # Remove trailing whitespaces
-    if [ "$UNAME" == "Darwin" ]
-    then
-        sed -i "" 's/[t ]*$//g' "${NEW_LICENSES_FILENAME}"
-    else
-        sed -i 's/[t ]*$//g' "${NEW_LICENSES_FILENAME}"
-    fi
-
-    deactivate
-
-    if [ $CHECK -eq 1 ]
-    then
-
-        echo "$DIFF_TOOL $LICENSES_FILENAME ${NEW_LICENSES_FILENAME}"
-        $DIFF_TOOL "$LICENSES_FILENAME" "${NEW_LICENSES_FILENAME}"
-        echo "Success: no update in $LICENSES_FILENAME"
     fi
 fi
 
