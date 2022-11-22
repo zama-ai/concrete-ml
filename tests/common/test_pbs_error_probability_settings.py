974414b3187e11e5e17ea4b54045fc5573f2ed8c
Benoit Chevallier-Mames <benoit.chevalliermames@zama.ai>
2022-11-22 19:18:22 +0100

    chore: factorize sklearn tests.

--- tests/common/test_pbs_error_probability_settings.py
+++ tests/common/test_pbs_error_probability_settings.py
@@ -1,16 +1,14 @@
 """Tests for the sklearn linear models."""
 import warnings
-
 from inspect import signature
-from concrete.ml.torch.compile import compile_torch_model
 
 import numpy
 import pytest
 from sklearn.exceptions import ConvergenceWarning
+from torch import nn
 
 from concrete.ml.pytest.utils import classifiers, regressors, sanitize_test_and_train_datasets
-
-from torch import nn
+from concrete.ml.torch.compile import compile_torch_model
 
 INPUT_OUTPUT_FEATURE = [5, 10]
 
@@ -34,35 +32,19 @@ class FC(nn.Module):
 
 
 @pytest.mark.parametrize("model, parameters", classifiers + regressors)
-@pytest.mark.parametrize("kwargs", [{"p_error": 0.03}, {"p_error": 0.04, "global_p_error": None}])
-def test_p_error(model, parameters, kwargs, load_data):
-    """Testing with p_error."""
-    x, y = load_data(**parameters)
-    model_params, x, _, x_train, y_train, _ = sanitize_test_and_train_datasets(model, x, y)
-
-    clf = model(**model_params)
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=ConvergenceWarning)
-        # Fit the model
-        clf.fit(x_train, y_train)
-
-    clf.compile(x_train, verbose_compilation=True, **kwargs)
-
-    # FIXME: waiting for https://github.com/zama-ai/concrete-numpy-internal/issues/1737 and
-    # https://github.com/zama-ai/concrete-numpy-internal/issues/1738
-    #
-    # Will check that we have
-    #   3.000000e-02 error per pbs call
-    # in Optimizer config
-
-
-@pytest.mark.parametrize("model, parameters", classifiers + regressors)
 @pytest.mark.parametrize(
-    "kwargs", [{"global_p_error": 0.025}, {"global_p_error": 0.036, "p_error": None}]
+    "kwargs",
+    [
+        {"p_error": 0.03},
+        {"p_error": 0.04, "global_p_error": None},
+        {"global_p_error": 0.025},
+        {"global_p_error": 0.036, "p_error": None},
+        {},
+        {"global_p_error": 0.038, "p_error": 0.39},
+    ],
 )
-def test_global_p_error(model, parameters, kwargs, load_data):
-    """Testing with global_p_error."""
+def test_config_sklearn(model, parameters, kwargs, load_data):
+    """Testing with p_error and global_p_error configs with sklearn models."""
 
     x, y = load_data(**parameters)
     x_train = x[:-1]
@@ -77,63 +59,17 @@ def test_global_p_error(model, parameters, kwargs, load_data):
         # Fit the model
         clf.fit(x_train, y_train)
 
-    clf.compile(x_train, verbose_compilation=True, **kwargs)
+    if kwargs.get("p_error", None) is not None and kwargs.get("global_p_error", None) is not None:
+        with pytest.raises(ValueError) as excinfo:
+            clf.compile(x_train, verbose_compilation=True, **kwargs)
+        assert "Please only set one of (p_error, global_p_error) values" in str(excinfo.value)
+    else:
+        clf.compile(x_train, verbose_compilation=True, **kwargs)
 
     # FIXME: waiting for https://github.com/zama-ai/concrete-numpy-internal/issues/1737 and
     # https://github.com/zama-ai/concrete-numpy-internal/issues/1738
     #
-    # Will check that we have
-    #   2.500000e-02 error per circuit call
-    # in Optimizer config
-
-
-@pytest.mark.parametrize("model, parameters", classifiers + regressors)
-def test_global_p_error_and_p_error_together(model, parameters, load_data):
-    """Testing with both p_error and global_p_error."""
-
-    x, y = load_data(**parameters)
-    x_train = x[:-1]
-    y_train = y[:-1]
-
-    model_params, x, _, x_train, y_train, _ = sanitize_test_and_train_datasets(model, x, y)
-
-    clf = model(**model_params)
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=ConvergenceWarning)
-        # Fit the model
-        clf.fit(x_train, y_train)
-
-    with pytest.raises(ValueError) as excinfo:
-        clf.compile(x_train, verbose_compilation=True, global_p_error=0.025, p_error=0.017)
-
-    assert "Please only set one of (p_error, global_p_error) values" in str(excinfo.value)
-
-
-@pytest.mark.parametrize("model, parameters", classifiers + regressors)
-def test_default(model, parameters, load_data):
-    """Testing with default."""
-
-    x, y = load_data(**parameters)
-    x_train = x[:-1]
-    y_train = y[:-1]
-
-    model_params, x, _, x_train, y_train, _ = sanitize_test_and_train_datasets(model, x, y)
-
-    clf = model(**model_params)
-
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", category=ConvergenceWarning)
-        # Fit the model
-        clf.fit(x_train, y_train)
-
-    clf.compile(x_train, verbose_compilation=True)
-
-    # FIXME: waiting for https://github.com/zama-ai/concrete-numpy-internal/issues/1737 and
-    # https://github.com/zama-ai/concrete-numpy-internal/issues/1738
-    #
-    # Will check that we have CN default
-    # in Optimizer config
+    # Will check that we have expected probabilities
 
 
 @pytest.mark.parametrize(
@@ -151,7 +87,7 @@ def test_default(model, parameters, load_data):
         {"global_p_error": 0.038, "p_error": 0.39},
     ],
 )
-def test_config_torch(model, kwargs, load_data):
+def test_config_torch(model, kwargs):
     """Testing with p_error and global_p_error configs with torch models."""
 
     input_output_feature = 5
@@ -171,12 +107,7 @@ def test_config_torch(model, kwargs, load_data):
         else numpy.random.uniform(-100, 100, size=(n_examples, input_output_feature))
     )
 
-    if (
-        "global_p_error" in kwargs
-        and "p_error" in kwargs
-        and kwargs["global_p_error"] is not None
-        and kwargs["p_error"] is not None
-    ):
+    if kwargs.get("p_error", None) is not None and kwargs.get("global_p_error", None) is not None:
         with pytest.raises(ValueError) as excinfo:
             compile_torch_model(
                 torch_model,
