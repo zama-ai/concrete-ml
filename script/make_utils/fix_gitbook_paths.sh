278a5984191edebe74053c5089b9d6349fecaf65
Benoit Chevallier-Mames <benoit.chevalliermames@zama.ai>
2023-01-12 17:27:36 +0100

    chore: when we release, the path is different
    closes #2698

--- script/make_utils/fix_gitbook_paths.sh
+++ script/make_utils/fix_gitbook_paths.sh
@@ -6,10 +6,20 @@ DIRECTORY=$1
 
 FILES=$(find "$DIRECTORY" -name "*.md")
 
+# Courtesy of https://stackoverflow.com/questions/6245570/how-do-i-get-the-current-branch-name-in-git
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
 for f in ${FILES}
 do
     # sed -i is different on mac and on linux
-    sed "s@../developer-guide/api@https://github.com/zama-ai/concrete-ml-internal/tree/main/docs/developer-guide/api@g" "$f" > tmp.file.md
+    if [[ "$CURRENT_BRANCH" == "release/"* ]]; then
+        FILLER="tree/$CURRENT_BRANCH"
+    else
+        FILLER="tree/main"
+    fi
+
+    sed "s@../developer-guide/api@https://github.com/zama-ai/concrete-ml-internal/$FILLER/docs/developer-guide/api@g" "$f" > tmp.file.md
+
     mv tmp.file.md "$f"
 
     # sed -i is different on mac and on linux
