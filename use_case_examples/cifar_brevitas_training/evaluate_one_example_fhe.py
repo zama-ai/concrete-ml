0f8def63db13e414bc2bd1e496419a186cd2a4f5
Roman Bredehoft <roman.bredehoft@zama.ai>
2023-08-29 18:59:52 +0200

    chore: clean and fix cifar training use case

--- use_case_examples/cifar_brevitas_training/evaluate_one_example_fhe.py
+++ use_case_examples/cifar_brevitas_training/evaluate_one_example_fhe.py
@@ -18,10 +18,10 @@ from concrete.ml.torch.compile import compile_brevitas_qat_model
 CURRENT_DIR = Path(__file__).resolve().parent
 KEYGEN_CACHE_DIR = CURRENT_DIR.joinpath(".keycache")
 
-# MPS option is supported by macOS with Apple Silicon or AMD GPUs
-DEVICE = (
-    "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-)
+# Add MPS (for macOS with Apple Silicon or AMD GPUs) support when error is fixed. For now, we
+# observe a decrease in torch's top1 accuracy when using MPS devices
+# FIXME: https://github.com/zama-ai/concrete-ml-internal/issues/3953
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 NUM_SAMPLES = int(os.environ.get("NUM_SAMPLES", 1))
 
 
