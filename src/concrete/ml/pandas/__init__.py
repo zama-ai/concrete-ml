c59a5a75247b6ff3414b5b527d6caaba45a85f83
Roman Bredehoft <roman.bredehoft@zama.ai>
2024-03-21 10:35:22 +0100

    chore: rename func and method names

--- src/concrete/ml/pandas/__init__.py
+++ src/concrete/ml/pandas/__init__.py
@@ -1,9 +1,9 @@
-from .client_server import get_client_and_eval_keys, load_client, load_server
+from .client_server import get_client_and_eval_keys, load_client
 from .dataframe import EncryptedDataFrame
 
 
-def encrypt_from_pandas(pandas_df, client, evaluation_keys):
-    return EncryptedDataFrame.encrypt_from_pandas(pandas_df, client, evaluation_keys)
+def encrypt_from_pandas(pandas_dataframe, client, evaluation_keys):
+    return EncryptedDataFrame.encrypt_from_pandas(pandas_dataframe, client, evaluation_keys)
 
 
 def encrypt_from_csv(file_path, client, evaluation_keys, **pandas_kwargs):
@@ -15,18 +15,18 @@ def load_encrypted_dataframe(file_path):
 
 
 def join(
-    encrypted_df_left: EncryptedDataFrame,
-    encrypted_df_right: EncryptedDataFrame,
+    left_encrypted: EncryptedDataFrame,
+    right_encrypted: EncryptedDataFrame,
     evaluation_keys,
     **pandas_kwargs
 ):
-    return encrypted_df_left.join(encrypted_df_right, evaluation_keys, **pandas_kwargs)
+    return left_encrypted.join(right_encrypted, evaluation_keys, **pandas_kwargs)
 
 
 def merge(
-    encrypted_df_left: EncryptedDataFrame,
-    encrypted_df_right: EncryptedDataFrame,
+    left_encrypted: EncryptedDataFrame,
+    right_encrypted: EncryptedDataFrame,
     evaluation_keys,
     **pandas_kwargs
 ):
-    return encrypted_df_left.merge(encrypted_df_right, evaluation_keys, **pandas_kwargs)
+    return left_encrypted.merge(right_encrypted, evaluation_keys, **pandas_kwargs)
