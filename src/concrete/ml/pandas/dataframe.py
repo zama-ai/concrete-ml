c7663a1151a41c0f948c9b12c92683b7dbafa16e
Roman Bredehoft <roman.bredehoft@zama.ai>
2024-03-21 19:14:17 +0100

    chore: add dtypes pre and post processing

--- src/concrete/ml/pandas/dataframe.py
+++ src/concrete/ml/pandas/dataframe.py
@@ -7,6 +7,7 @@ import pandas
 
 from concrete import fhe
 from concrete.ml.pandas._operators import encrypted_merge
+from concrete.ml.pandas._processing import post_process_to_pandas, pre_process_from_pandas
 from concrete.ml.pandas._utils import (
     decrypt_elementwise,
     deserialize_elementwise,
@@ -18,74 +19,9 @@ from concrete.ml.pandas._utils import (
     serialize_evaluation_keys,
     serialize_value,
 )
-from concrete.ml.pandas.client_server import _SERVER_PATH, _get_encrypt_config, _get_min_max_allowed
+from concrete.ml.pandas.client_server import _SERVER_PATH, _get_encrypt_config
 
 _SERVER = fhe.Server.load(_SERVER_PATH)
-
-
-def _validate_pandas_dataframe(pandas_dataframe: pandas.DataFrame, min_value: int, max_value: int):
-    """Check that the data-frame only contains values between the given min/max."""
-
-    non_integer_columns = list(
-        pandas_dataframe.columns[
-            ~pandas_dataframe.dtypes.apply(lambda x: numpy.issubdtype(x, numpy.integer))
-        ]
-    )
-    if non_integer_columns:
-        raise ValueError(
-            f"Columns {non_integer_columns} contain non-integer values, which is currently noy "
-            "allowed."
-        )
-
-    columns_less_than_min = (pandas_dataframe < min_value).any()
-    column_names_less_than_min = columns_less_than_min[columns_less_than_min].index.tolist()
-
-    if column_names_less_than_min:
-        raise ValueError(
-            f"Columns {column_names_less_than_min} contain values less than {min_value}, "
-            "which is currently not allowed."
-            + " This is because 0 values are used to represent NaN values for FHE computations."
-            * (min_value <= 1)
-        )
-
-    columns_greater_than_max = (pandas_dataframe > max_value).any()
-    column_names_greater_than_max = columns_greater_than_max[
-        columns_greater_than_max
-    ].index.tolist()
-
-    if column_names_greater_than_max:
-        raise ValueError(
-            f"Columns {column_names_greater_than_max} contain values greater than {max_value}, "
-            "which is currently not allowed."
-        )
-
-
-def _pre_process_from_pandas(pandas_dataframe: pandas.DataFrame) -> numpy.ndarray:
-    """Pre-process the Pandas data-frame."""
-    # Make sure the given data-frame only contains integer values between the allowed min and max
-    # Additionally, forbid 0 values as they are then used to represent encrypted NaN values
-    min, max = _get_min_max_allowed()
-    _validate_pandas_dataframe(pandas_dataframe, min, max)
-
-    # Replace NaN values with 0
-    pandas_dataframe.fillna(0, inplace=True)
-
-    pandas_array = pandas_dataframe.to_numpy()
-
-    return pandas_array
-
-
-def _post_process_to_pandas(clear_array: numpy.ndarray, output_column_names: List[str]):
-    """Post-process the server's outputs and build a Pandas data-frame from them."""
-    # Replace 0 values by NaN
-    clear_array_0_to_nan = numpy.where(clear_array == 0, numpy.nan, clear_array)
-
-    # Convert the array with a pandas data-frame
-    df = pandas.DataFrame(
-        clear_array_0_to_nan,
-        columns=output_column_names,
-    )
-    return df
 
 
 class EncryptedDataFrame:
@@ -97,12 +33,39 @@ class EncryptedDataFrame:
         encrypted_nan: fhe.Value,
         evaluation_keys: fhe.EvaluationKeys,
         column_names: List[str],
+        dtype_mappings: Dict,
     ):
-        self.encrypted_values = encrypted_values
-        self.encrypted_nan = encrypted_nan
-        self.evaluation_keys = evaluation_keys
-        self.column_names = list(column_names)
-        self.column_names_to_index = {name: index for index, name in enumerate(column_names)}
+        self._encrypted_values = encrypted_values
+        self._encrypted_nan = encrypted_nan
+        self._evaluation_keys = evaluation_keys
+
+        self._column_names = list(column_names)
+        self._column_names_to_index = {name: index for index, name in enumerate(column_names)}
+        self._dtype_mappings = dtype_mappings
+
+    @property
+    def encrypted_values(self):
+        return self._encrypted_values
+
+    @property
+    def encrypted_nan(self):
+        return self._encrypted_nan
+
+    @property
+    def evaluation_keys(self):
+        return self._evaluation_keys
+
+    @property
+    def column_names(self):
+        return self._column_names
+
+    @property
+    def column_names_to_index(self):
+        return self._column_names_to_index
+
+    @property
+    def dtype_mappings(self):
+        return self._dtype_mappings
 
     def join(
         self,
@@ -166,7 +129,7 @@ class EncryptedDataFrame:
         For now, only a left and right join merge is supported. Additionally, default values for
         parameters 'how' and 'validate' are different than in Pandas.
         """
-        joined_array, joined_column_names = encrypted_merge(
+        joined_array, joined_column_names, joined_dtype_mappings = encrypted_merge(
             self,
             other,
             _SERVER,
@@ -184,20 +147,30 @@ class EncryptedDataFrame:
         )
 
         joined_df = EncryptedDataFrame(
-            joined_array, self.encrypted_nan, self.evaluation_keys, joined_column_names
+            joined_array,
+            self._encrypted_nan,
+            self._evaluation_keys,
+            joined_column_names,
+            joined_dtype_mappings,
         )
 
         return joined_df
 
     @classmethod
     def encrypt_from_pandas(cls, pandas_dataframe, client, evaluation_keys):
-        pandas_array = _pre_process_from_pandas(pandas_dataframe)
+        pandas_array, dtype_mappings = pre_process_from_pandas(pandas_dataframe)
 
         # TODO: how to provide encrypt configuration
         encrypted_values = encrypt_elementwise(pandas_array, client, **_get_encrypt_config())
         encrypted_nan = encrypt_value(0, client, **_get_encrypt_config())
 
-        return cls(encrypted_values, encrypted_nan, evaluation_keys, pandas_dataframe.columns)
+        return cls(
+            encrypted_values,
+            encrypted_nan,
+            evaluation_keys,
+            pandas_dataframe.columns,
+            dtype_mappings,
+        )
 
     @classmethod
     def encrypt_from_csv(cls, file_path, client, evaluation_keys, **pandas_kwargs):
@@ -205,8 +178,10 @@ class EncryptedDataFrame:
         return cls.encrypt_from_pandas(pandas_dataframe, client, evaluation_keys)
 
     def decrypt_to_pandas(self, client):
-        clear_array = decrypt_elementwise(self.encrypted_values, client)
-        pandas_dataframe = _post_process_to_pandas(clear_array, self.column_names)
+        clear_array = decrypt_elementwise(self._encrypted_values, client)
+        pandas_dataframe = post_process_to_pandas(
+            clear_array, self._column_names, self._dtype_mappings
+        )
         return pandas_dataframe
 
     def decrypt_to_csv(self, file_path, **pandas_kwargs):
@@ -215,16 +190,17 @@ class EncryptedDataFrame:
 
     def to_dict(self):
         """Serialize the instance to a dictionary."""
-        encrypted_values = serialize_elementwise(self.encrypted_values)
-        encrypted_nan = serialize_value(self.encrypted_nan)
-        evaluation_keys = serialize_evaluation_keys(self.evaluation_keys)
+        encrypted_values = serialize_elementwise(self._encrypted_values)
+        encrypted_nan = serialize_value(self._encrypted_nan)
+        evaluation_keys = serialize_evaluation_keys(self._evaluation_keys)
 
         # A Numpy array is not serializable using JSON so we need to convert to a list
         output_dict = {
             "encrypted_values": encrypted_values.tolist(),
             "encrypted_nan": encrypted_nan,
             "evaluation_keys": evaluation_keys,
-            "column_names": self.column_names,
+            "column_names": self._column_names,
+            "dtype_mappings": self._dtype_mappings,
         }
 
         return output_dict
@@ -236,8 +212,9 @@ class EncryptedDataFrame:
         encrypted_nan = deserialize_value(dict_to_load["encrypted_nan"])
         evaluation_keys = deserialize_evaluation_keys(dict_to_load["evaluation_keys"])
         column_names = dict_to_load["column_names"]
+        dtype_mappings = dict_to_load["dtype_mappings"]
 
-        return cls(encrypted_values, encrypted_nan, evaluation_keys, column_names)
+        return cls(encrypted_values, encrypted_nan, evaluation_keys, column_names, dtype_mappings)
 
     def to_json(self, file_path):
         file_path = Path(file_path)
