b0b9f50fb742e212b43a654e2d966f1f0e0d657e
Roman Bredehoft <roman.bredehoft@zama.ai>
2024-03-28 18:14:09 +0100

    chore: move tests with Pandas data-frame as input

--- tests/sklearn/test_sklearn_models.py
+++ tests/sklearn/test_sklearn_models.py
@@ -24,6 +24,7 @@ Generic tests test:
 import copy
 import json
 import os
+import sys
 import tempfile
 
 # pylint: disable=too-many-lines, too-many-arguments
@@ -2014,3 +2015,41 @@ def test_valid_n_bits_setting(
         # Sometimes, we miss convergence, which is not a problem for our test
         warnings.simplefilter("ignore", category=ConvergenceWarning)
         model.fit(x, y)
+
+
+# A type error will be raised for NeuralNetworks, which is tested in test_failure_bad_data_types
+@pytest.mark.parametrize(
+    "model_class",
+    _get_sklearn_linear_models() + _get_sklearn_tree_models() + _get_sklearn_neighbors_models(),
+)
+@pytest.mark.parametrize(
+    "bad_value, expected_error",
+    [
+        (numpy.nan, "Input X contains NaN."),
+        (None, "Input X contains NaN."),
+        ("this", "could not convert string to float: 'this'"),
+    ],
+)
+def test_error_raise_unsupported_pandas_values(model_class, bad_value, expected_error):
+    """Test that using Pandas data-frame with unsupported values as input raises correct errors."""
+
+    dic = {
+        "Col One": [1, 2, bad_value, 3],
+        "Col Two": [4, 5, 6, bad_value],
+        "Col Three": [bad_value, 7, 8, 9],
+    }
+
+    # Creating a dataframe using dictionary
+    x_train = pandas.DataFrame(dic)
+    y_train = x_train["Col Three"]
+
+    model = model_class(n_bits=2)
+
+    # The error message changed in one of our dependencies
+    assert sys.version_info.major == 3
+    if sys.version_info.minor <= 7:
+        if expected_error == "Input X contains NaN.":
+            expected_error = "Input contains NaN*"
+
+    with pytest.raises(ValueError, match=expected_error):
+        model.fit(x_train, y_train)
