5306f6cca65b551f65cb92203e53bcbc988131af
Jordan Fréry <jordan.frery@zama.ai>
2024-04-03 11:13:42 +0200

    chore: update cifar training readme + accuracy eval (#572)

--- use_case_examples/cifar/cifar_brevitas_training/evaluate_torch_cml.py
+++ use_case_examples/cifar/cifar_brevitas_training/evaluate_torch_cml.py
@@ -118,7 +118,9 @@ def main(args):
             input_set,
             n_bits={"model_inputs": 8, "model_outputs": 8},
             configuration=cfg,
-            rounding_threshold_bits=rounding_threshold_bits,
+            rounding_threshold_bits={"n_bits": rounding_threshold_bits, "method": "EXACT"}
+            if rounding_threshold_bits is not None
+            else None,
         )
 
         # Print max bit-width in the circuit
