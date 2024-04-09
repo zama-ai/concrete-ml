e911db37ae34a313222d928ec70fddbe0f737a41
Roman <56846628+RomanBredehoft@users.noreply.github.com>
2024-04-09 10:22:53 +0200

    chore: improve license update action (#593)

--- script/make_utils/fix_omp_issues_for_intel_mac.sh
+++ script/make_utils/fix_omp_issues_for_intel_mac.sh
@@ -7,8 +7,10 @@ set -ex
 
 UNAME=$(uname)
 MACHINE=$(uname -m)
+PYTHON_VERSION=$(python --version | cut -d' ' -f2 | cut -d'.' -f1,2)
 
-if [ "$UNAME" == "Darwin" ] && [ "$MACHINE" != "arm64" ]
+# The error does not seem to happen on python 3.10 (on MacOS 13.6.6)
+if [ "$UNAME" == "Darwin" ] && [ "$MACHINE" != "arm64" ] && [ "$PYTHON_VERSION" != "3.10" ]
 then
 
     # We need to source the venv here, since it's not done in the CI
