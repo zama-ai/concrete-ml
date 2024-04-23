34059788e8d3cca1aed3be424e4d823da3acf79a
Andrei Stoian <95410270+andrei-stoian-zama@users.noreply.github.com>
2024-04-23 10:16:07 +0200

    chore: remove sphinx doc build (#631)

--- script/make_utils/sphinx_gitbook_admonitions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-OPTIONS=$1
-
-MD_FILES=$(find docs-copy -type f -name "*.md")
-
-# shellcheck disable=SC2086
-poetry run python script/doc_utils/sphinx_gitbook_admonitions.py --files $MD_FILES $OPTIONS
