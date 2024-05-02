0891e8d6c3c0be97ea7c6fd21184bde3e91ea131
jfrery <jordan.frery@zama.ai>
2024-05-02 15:14:28 +0200

    chore: add fixme

--- tests/sklearn/test_sklearn_models.py
+++ tests/sklearn/test_sklearn_models.py
@@ -2045,7 +2045,8 @@ def test_error_raise_unsupported_pandas_values(model_class, bad_value, expected_
     with pytest.raises(ValueError, match=expected_error):
         model.fit(x_train, y_train)
 
-
+# Add QNNs in this test
+# FIXME: https://github.com/zama-ai/concrete-ml-internal/issues/4436
 @pytest.mark.parametrize(
     "model_class, parameters",
     get_sklearn_linear_models_and_datasets()
