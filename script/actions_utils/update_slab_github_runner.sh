c74a9db7fce24136f6b7758c1087a7753bb92c12
kcelia <celia.kherfallah@zama.ai>
2024-12-19 16:49:32 +0100

    chore: update slab github runner

--- script/actions_utils/update_slab_github_runner.sh
+++ script/actions_utils/update_slab_github_runner.sh
@@ -10,14 +10,14 @@ RELEASE_COMMIT="11bd71901bbe5b1630ceea73d27597364c9af683"
 for file in $(find .github -type f \( -name "*.yml" -o -name "*.yaml" \)); do
     echo "Processing: $file"
 
-    # Extract lines containing "uses: actions/checkout@"
+    # Extract lines containing "uses: zama-ai/slab-github-runner@"
     echo "Before modifications:"
-    grep -nE "uses: actions/checkout@" "$file"
+    grep -nE "uses: zama-ai/slab-github-runner@" "$file"
 
     # Replace the target line with the new commit, any comments will be deleted
-    sed -E -i "s|uses: actions/checkout@.*|uses: actions/checkout@$RELEASE_COMMIT|g" "$file"
+    sed -E -i "s|uses: zama-ai/slab-github-runner@.*|uses: zama-ai/slab-github-runner@$RELEASE_COMMIT|g" "$file"
 
-    # Extract lines containing "uses: actions/checkout@" after modification
+    # Extract lines containing "uses: zama-ai/slab-github-runner@" after modification
     echo "After modifications:"
-    grep -nE "uses: actions/checkout@" "$file"
+    grep -nE "uses: zama-ai/slab-github-runner@" "$file"
 done
