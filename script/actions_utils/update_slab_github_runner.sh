f97404023cf7ec79249aefcd9b235cbfc0f3826c
kcelia <celia.kherfallah@zama.ai>
2024-12-19 16:40:23 +0100

    chore: update the regular expression so that we remove any comments after the previous commit

--- script/actions_utils/update_slab_github_runner.sh
+++ script/actions_utils/update_slab_github_runner.sh
@@ -7,23 +7,17 @@
 # Define the release commit to replace
 RELEASE_COMMIT="f26b8d611b2e695158fb0a6980834f0612f65ef8"
 
-echo "Current directory: $(pwd)"
-
-# Search for files and process them
 for file in $(find .github -type f \( -name "*.yml" -o -name "*.yaml" \)); do
     echo "Processing: $file"
 
     # Extract lines containing "uses: actions/checkout@"
     echo "Before modifications:"
-    grep -nE "uses: actions/checkout@" "$file" || echo "No match found"
+    grep -nE "uses: actions/checkout@" "$file"
 
-    # Replace the target line with the new commit, keeping indentation intact
-    sed -E -i "s|(uses: actions/checkout@)[^[:space:]]*|\1$RELEASE_COMMIT|g" "$file"
+    # Replace the target line with the new commit, any comments will be deleted
+    sed -E -i "s|uses: actions/checkout@.*|uses: actions/checkout@$RELEASE_COMMIT|g" "$file"
 
     # Extract lines containing "uses: actions/checkout@" after modification
     echo "After modifications:"
-    grep -nE "uses: actions/checkout@" "$file" || echo "No match found"
-
-    echo "Updated: $file"
-    echo "-------------------------"
+    grep -nE "uses: actions/checkout@" "$file"
 done
