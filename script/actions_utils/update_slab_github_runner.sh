ca3bac8a2f3ea0958862778577a36ed48f45577c
kcelia <celia.kherfallah@zama.ai>
2024-12-19 16:47:17 +0100

    chore: mistake

--- script/actions_utils/update_slab_github_runner.sh
+++ script/actions_utils/update_slab_github_runner.sh
@@ -5,7 +5,7 @@
 # Note: For security reasons, we prefer updating the commit instead of the tag.
 
 # Define the release commit to replace
-RELEASE_COMMIT="f26b8d611b2e695158fb0a6980834f0612f65ef8"
+RELEASE_COMMIT="11bd71901bbe5b1630ceea73d27597364c9af683"
 
 for file in $(find .github -type f \( -name "*.yml" -o -name "*.yaml" \)); do
     echo "Processing: $file"
