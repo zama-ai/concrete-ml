e05c869fa5530db23ef0c8385dda38b73c9ddaf5
Andrei Stoian <andrei.stoian@zama.ai>
2025-01-08 09:58:30 +0100

    fix: poetry version

--- script/make_utils/setup_os_deps.sh
+++ script/make_utils/setup_os_deps.sh
@@ -174,7 +174,7 @@ if [[ "${OS_NAME}" == "Linux" ]]; then
 
     # Install poetry, either with pipx for ubuntu >= 23
     # or through regular pip for older ubuntu
-    (pipx install poetry && pipx ensurepath) || \
+    (pipx install poetry==1.8.4 && pipx ensurepath) || \
     (\
         python3 -m pip install --no-cache-dir --upgrade pip && \
         python3 -m pip install --no-cache-dir --ignore-installed poetry==1.8.4 \
