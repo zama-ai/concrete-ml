3754cf62dc46a069647050e8b53bda20ba47ce38
VolodymyrBg <aqdrgg19@gmail.com>
2025-02-21 22:32:23 +0200

    fix: restore AdaptiveAvgPool2d in ResNet model

--- use_case_examples/resnet/resnet.py
+++ use_case_examples/resnet/resnet.py
@@ -213,8 +213,7 @@ class ResNet(nn.Module):
         self.layer4 = self._make_layer(
             block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2]
         )
-        # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # FIXME
-        self.avgpool = nn.AvgPool2d(kernel_size=7, stride=1, padding=0)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
